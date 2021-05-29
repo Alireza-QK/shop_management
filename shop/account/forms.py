674d63ae1db09b17893e5332d813839a8b4f5888
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-29 16:36:20 +0430

    make form UserRegisterForm

--- shop/account/forms.py
+++ shop/account/forms.py
@@ -1,12 +1,18 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
-class RegisterUserForm(forms.ModelForm):
+class RegisterUserForm(UserCreationForm):
 
-    def __init__(self):
-        pass
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.fields['username'].help_text = ''
+		self.fields['password1'].help_text = ''
+		self.fields['password2'].help_text = ''
+		for visible in self.visible_fields():
+			visible.field.widget.attrs['class'] = 'form-control'
 
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'password')
-        
+	class Meta:
+		model = User
+		fields = ('first_name', 'last_name', 'username', 'email', 'username')
+	
