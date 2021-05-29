f64adcb05182bd9f7adcfc9c5e5620fde1dbc3aa
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-29 11:49:06 +0430

    Make model User

--- shop/account/models.py
+++ shop/account/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    avatar_profile = models.ImageField('avatar profile', upload_to='account/avatars/')
+    phone_number = models.CharField(max_length=12)
+    address = models.TextField(blank=True)
+
+
+    class Meta:
+        db_table = 'account_user'
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
