bc78df2aed0f7441a50af784be33b15fa150bbfe
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-29 16:36:36 +0430

    add view regiser user

--- shop/account/views.py
+++ shop/account/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.views.generic import CreateView, FormView
 from django.contrib.auth.views import LoginView
 from .models import User
+from .forms import RegisterUserForm
 
 
-class UserRegisterView(LoginView):
+class UserRegisterView(CreateView):
     model = User
     template_name = 'account/auth/register.html'
-    # form_class = User
-    fields = ('username', 'email', 'phone_number')
+    form_class = RegisterUserForm
+
