13851c72b21a55d08ad4a353017259644442cdbf
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-30 12:35:54 +0430

    add path login to urls.py

--- shop/account/urls.py
+++ shop/account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserRegisterView, activate
+from .views import UserRegisterView, UserLoginView, activate
 
 app_name = 'account'
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name="register"),
+    path('login/', UserLoginView.as_view(), name="login"),
     path('activate/<uidb64>/<token>/', activate, name='activate'),
 ]
