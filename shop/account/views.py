2d57efd352ff4b450a8848ceefda92fdc92873ab
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-30 11:51:18 +0430

    register account with veryfiction

--- shop/account/views.py
+++ shop/account/views.py
@@ -14,7 +14,55 @@ from .utils import account_activation_token
 
 
 class UserRegisterView(CreateView):
-    model = User
-    template_name = 'account/auth/register.html'
-    form_class = RegisterUserForm
+	model = User
+	template_name = 'account/auth/register.html'
+	form_class = RegisterUserForm
 
+	def form_valid(self, form):
+		response = form.save(commit=False)
+		context = {'form': None}
+
+		if form.is_valid():
+			first_name = form.cleaned_data.get('first_name')
+			last_name = form.cleaned_data.get('last_name')
+			username = form.cleaned_data.get('username')
+			email = form.cleaned_data.get('email')
+			phone_number = form.cleaned_data.get('phone_number')
+			response.is_active = False
+			form.save()
+			current_site = get_current_site(self.request)
+			mail_subject = 'تایئد حساب کاربری'
+			message = render_to_string('account/auth/account_activaction.html', {
+				'user': response,
+				'domain': current_site.domain,
+				'uid': urlsafe_base64_encode(force_bytes(response.pk)),
+				'token': account_activation_token.make_token(response),
+			})
+			to_email = form.cleaned_data.get('email')
+			email = EmailMessage(
+				mail_subject, message, to=[to_email]
+			)
+			email.send()
+			return HttpResponse('Please confirm your email address to complete the registration')
+		else:
+			form = form
+		
+		context = {
+			'form': form
+		}
+		
+		return render(self.request, self.template_name, context)
+
+
+def activate(request, uidb64, token):
+	try:
+		uid = urlsafe_base64_decode(uidb64).decode()
+		user = User.objects.get(pk=uid)
+	except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+		user = None
+	if user is not None and account_activation_token.check_token(user, token):
+		user.is_active = True
+		user.save()
+		return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+	else:
+		return HttpResponse('Activation link is invalid!')
