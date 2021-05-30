2e1d8bf20f2fad0171d54796530305fae3101ba3
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-30 16:09:00 +0430

    init model Product

--- shop/product/models.py
+++ shop/product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=254)
+    text = models.TextField()
+    amount = models.BigIntegerField(verbose_name='قیمت', default=0)
+    discount = models.BigIntegerField(verbose_name='قیمت با تخفیف', default=0)
+    image = models.ImageField(verbose_name='تصویر', upload_to='product/images/')
+    stock_count = models.IntegerField(verbose_name='تعداد موجودی', default=1)
+    sell_count = models.IntegerField(verbose_name='تعداد به فروش رفته', default=0)
