876f32d2557caa55aaa00d12f1643c56f508ec90
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-31 20:06:08 +0430

    Make model GalleruProduct

--- shop/product/models.py
+++ shop/product/models.py
@@ -2,10 +2,20 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=254)
-    text = models.TextField()
-    amount = models.BigIntegerField(verbose_name='قیمت', default=0)
-    discount = models.BigIntegerField(verbose_name='قیمت با تخفیف', default=0)
-    image = models.ImageField(verbose_name='تصویر', upload_to='product/images/')
-    stock_count = models.IntegerField(verbose_name='تعداد موجودی', default=1)
-    sell_count = models.IntegerField(verbose_name='تعداد به فروش رفته', default=0)
+	title = models.CharField(max_length=254, verbose_name='عنوام محصول')
+	text = models.TextField()
+	price = models.BigIntegerField(verbose_name='قیمت', default=0)
+	discount = models.BigIntegerField(verbose_name='قیمت با تخفیف', default=0)
+	image = models.ImageField(verbose_name='تصویر', upload_to='product/images/')
+	stock_count = models.IntegerField(verbose_name='تعداد موجودی', default=1)
+	sell_count = models.IntegerField(verbose_name='تعداد به فروش رفته', default=0)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+
+class GalleryProduct(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='galleries')
+	images = models.ImageField(verbose_name='تصویر', upload_to='product/galleries/')
+
+	def __str__(self):
+		return self.product.title
