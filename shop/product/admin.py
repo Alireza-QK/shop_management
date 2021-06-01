eed9d0eb8441699fb474a337a309b57315537ecc
Alireza-QK <Alireza.QK.75@gmail.com>
2021-06-01 12:10:55 +0430

    add model ProductAdmin

--- shop/product/admin.py
+++ shop/product/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product, GalleryProduct
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = ('title', 'price', 'discount', 'created_at', 'updated_at')
+
+
+admin.site.register(Product, ProductAdmin)
