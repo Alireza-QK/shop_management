da63c595c0d14d54ad8501fe4dbd81da52c92c21
Alireza-QK <Alireza.QK.75@gmail.com>
2021-06-01 11:21:37 +0430

    add url product_create

--- shop/product/urls.py
+++ shop/product/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import ProductListView
+from .views import (
+	ProductListView,
+	ProductCreateView,
+)
 
 app_name = "product"
 
 urlpatterns = [
 	path('', ProductListView.as_view(), name="home"),
+	path('product_create', ProductCreateView.as_view(), name="product_create"),
 ]
