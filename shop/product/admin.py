621dad8275c95e201bccb4af87870a379efd2146
Alireza-QK <Alireza.QK.75@gmail.com>
2021-06-01 15:54:44 +0430

    add model GalleryProductAdmin to admin.py

--- shop/product/admin.py
+++ shop/product/admin.py
@@ -6,4 +6,9 @@ class ProductAdmin(admin.ModelAdmin):
 	list_display = ('title', 'price', 'discount', 'created_at', 'updated_at')
 
 
+class GalleryProductAdmin(admin.ModelAdmin):
+	list_display = ('product', )
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(GalleryProduct, GalleryProductAdmin)
