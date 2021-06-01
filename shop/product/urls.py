5a08c6b42507a2134344b8691f4821331a1f7bd2
Alireza-QK <Alireza.QK.75@gmail.com>
2021-06-01 15:35:11 +0430

    add url galley_create to urls.py

--- shop/product/urls.py
+++ shop/product/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
 	ProductListView,
 	ProductCreateView,
+	GalleryCreateView,
 )
 
 app_name = "product"
@@ -9,4 +10,8 @@ app_name = "product"
 urlpatterns = [
 	path('', ProductListView.as_view(), name="home"),
 	path('product_create/', ProductCreateView.as_view(), name="product_create"),
+]
+
+urlpatterns += [
+	path('galley_create/', GalleryCreateView.as_view(), name="gallery_create"),
 ]
