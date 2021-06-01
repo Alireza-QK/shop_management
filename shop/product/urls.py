08ab54130b893766015490b92209d65de19ca02c
Alireza-QK <Alireza.QK.75@gmail.com>
2021-06-02 02:15:39 +0430

    add url update product to ulrs.py

--- shop/product/urls.py
+++ shop/product/urls.py
@@ -3,6 +3,7 @@ from .views import (
 	ProductListView,
 	ProductDetailView,
 	ProductCreateView,
+	ProductUpdateView,
 	GalleryCreateView,
 )
 
@@ -12,6 +13,7 @@ urlpatterns = [
 	path('', ProductListView.as_view(), name="home"),
 	path('product/<int:pk>/', ProductDetailView.as_view(), name="detail"),
 	path('product_create/', ProductCreateView.as_view(), name="product_create"),
+	path('product_update/<int:pk>', ProductUpdateView.as_view(), name="product_update"),
 ]
 
 urlpatterns += [
