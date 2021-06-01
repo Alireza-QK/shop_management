8b3a260b1608918cc7ea4c1e87d59bc547ccb7c6
Alireza-QK <Alireza.QK.75@gmail.com>
2021-06-01 12:06:27 +0430

    added signals for pre_save,pre_delete in models for product

--- shop/product/models.py
+++ shop/product/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, pre_delete
+from django.conf import settings
+from PIL import Image
+from uuid import uuid4
 
 
 class Product(models.Model):
@@ -7,6 +12,7 @@ class Product(models.Model):
 	price = models.BigIntegerField(verbose_name='قیمت', default=0)
 	discount = models.BigIntegerField(verbose_name='قیمت با تخفیف', default=0)
 	image = models.ImageField(verbose_name='تصویر', upload_to='product/images/')
+	thumbnail = models.ImageField(default=None, blank=True, null=True)
 	stock_count = models.IntegerField(verbose_name='تعداد موجودی', default=1)
 	sell_count = models.IntegerField(verbose_name='تعداد به فروش رفته', default=0)
 	created_at = models.DateTimeField(auto_now_add=True)
@@ -19,3 +25,22 @@ class GalleryProduct(models.Model):
 
 	def __str__(self):
 		return self.product.title
+
+
+@receiver(pre_save, sender=Product)
+def pre_save_product(sender, instance, **kwargs):
+	image = Image.open(instance.image)
+	resized_image = image.resize((400, 400))
+	fileName = "%s.%s" % (uuid4(), image.format)
+	filePath = f"{settings.MEDIA_ROOT}/product/images/{fileName}"
+	instance.thumbnail = f"product/images/{filename}"
+	resized_image.save(filePath)
+
+
+@receiver(pre_delete, sender=Product)
+def pre_delete_product(sender, instance, **kwargs):
+	object_current = sender.objects.get(pk=instance.pk)
+
+	if object_current:
+		os.remove(object_current.image.path)
+		os.remove(object_current.thumbnail.path)
