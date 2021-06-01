6b5121db12d0cb14b507ccd767e30d6d168c01ca
Alireza-QK <Alireza.QK.75@gmail.com>
2021-06-01 11:20:10 +0430

    add view ProductCreateView

--- shop/product/views.py
+++ shop/product/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import (
+	ListView,
+	CreateView,
+)
 from .models import Product, GalleryProduct
+from .forms import ProductForm, GalleryProductForm
 
 
 class ProductListView(ListView):
 	model = Product
 	template_name = 'product/product_list.html'
+
+# Show detail product
+
+
+class ProductCreateView(CreateView):
+	model = Product
+	form_class = ProductForm
+	template_name = 'product/product_create.html'
